bffd933fc10f7554ad4130f78e2019af5ae96485
AbrahaMD1010 <amiguel@unal.edu.co>
2023-03-05 18:33:20 -0500

    ac2

--- src/main/java/classroom/Grupo.java
+++ src/main/java/classroom/Grupo.java
@@ -18,8 +18,8 @@ public class Grupo {
     }
 
     public Grupo(int cantidadEstudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
-        this(personas, profesor, asignatura, codigo, horario);
-        Persona[] personas = new Persona[cantidadEstudiantes];    
+        this(new Persona[cantidadEstudiantes], profesor, asignatura, codigo, horario);
+        //Persona[] personas = new Persona[cantidadEstudiantes];    
     }
 
     public Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura) {
--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -36,6 +36,8 @@ public class Persona {
     }
 
     public Persona() {
+        cedula = 1; 
+        this.nombre = "";
         totalPersonas++;
     }
     
